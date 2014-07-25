a3f80ff470667c716b371be3f436c110aac9db2a
Jeremy Cerise <jcerise06@gmail.com>
2014-07-24 23:42:32 -0600

    Added an animated menu header to the main menu using groups and tweening

--- game/states/menu.js
+++ game/states/menu.js
@@ -13,6 +13,28 @@ Menu.prototype = {
     // Add the ground sprite as a Tile, and start scrolling in the negative
     this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
     this.ground.autoScroll(-200, 0);
+
+    // Create a group to put the title assets in, so they can move as a whole
+    this.titleGroup = this.game.add.group();
+
+    // Add the title sprite, and add it to the title group
+    this.title = this.game.add.sprite(0, 0, 'title');
+    this.titleGroup.add(this.title);
+
+    // Create the bird sprite and add it to the title group
+    this.bird = this.game.add.sprite(200, 5, 'bird');
+    this.titleGroup.add(this.bird);
+
+    // Add and begin and animation on the bird
+    this.bird.animations.add('flap');
+    this.bird.animations.play('flap', 12, true);
+
+    // Set the originating location of the group
+    this.titleGroup.x = 30;
+    this.titleGroup.y = 100;
+
+    // Create an oscillating animation tween for the entire group
+    this.game.add.tween(this.titleGroup).to({y:115}, 350, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
   },
   update: function() {
 
